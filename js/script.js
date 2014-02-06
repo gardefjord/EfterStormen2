2f7888c8e41f9b592b51214d6a59a902275cec5c
Gardefjord <adamgardefjord@gmail.com>
2014-02-06 14:11:22 +0100

    Script to start movie on click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-	$('.start_movie').onclick
-	$('.bs-masthead').append( '<p>Test</p>' );
 
+	$('.trig_movie').click( function(e){
+		e.preventDefault();
+		var URL = $(this).attr('href');
+		var htm = '<iframe width="425" height="349" src="http://www.youtube.com/embed/' + URL + '?autoplay=1;3&amp;autohide=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
 
+		$('#vid_container').html(htm);
+
+		player.playVideo();
+		return false;
+	});
 
 });
+
